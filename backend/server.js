8a21389d8a1010c62db01b0a8d44205ef4c1590c
Linnovel <linnovel_88@hotmail.com>
2024-10-28 21:17:39 -0400

    post and deleted endpoints added and tested

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv'
 import { connectDB } from './config/db.js';
+import Product from './models/product.model.js';
 
 dotenv.config();
 
@@ -8,11 +9,46 @@ dotenv.config();
 //servidor
 const app = express()
 
+app.use(express.json()); //allow us to accept json data in the req.body
+
 
 //router
-app.get("/products", (req, res) => {
-    // res.send("Server is ready")
+//endpoints
+app.get("/api", (req, res) => {
+    res.send("Server is ready")
+    console.log(`Server is ready ${res}`)
+});
+
+// POST
+app.post("/api/products", async (req, res) => {
+    const product = req.body; //user body to create product
+
+    if (!product.name || !product.price || !product.image || !product.avaliable) {
+        return res.status(400).json({ success: false, message: 'Please provide all fields' })
+    }
+
+    const newProduct = new Product(product)
+
+    try {
+        await newProduct.save();
+        res.status(201).json({ success: true, data: newProduct })
+    } catch (error) {
+        console.log("Error in creating product", error.message)
+        res.status(500).json({ success: false, message: "Server Error" })
+    }
 })
+
+//Delete method
+app.delete("/api/products/:id", async (req, res) => {
+    const { id } = req.params
+    try {
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({ succes: true, message: "product deleted correctly" })
+    } catch (error) {
+
+    }
+})
+
 
 // console.log(process.env.MONGO_URL)
 
